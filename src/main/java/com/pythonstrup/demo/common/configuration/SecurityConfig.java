c5d71ee6f7299026cd4eaa993f790739f04ffad7
dohyeon-han <limited.hyeon@gmail.com>
2023-01-09 23:17:22 +0900

    feature: HttpSessionSecurityContextRepository 등록 및 UserDetails 권한 수정

--- src/main/java/com/pythonstrup/demo/common/configuration/SecurityConfig.java
+++ src/main/java/com/pythonstrup/demo/common/configuration/SecurityConfig.java
@@ -9,10 +9,14 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 // WebSecurityConfigurerAdapter가 deprecated됨.
@@ -34,13 +38,14 @@ public class SecurityConfig {
     @Bean
     @Order(SecurityProperties.BASIC_AUTH_ORDER)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        AuthenticationManager authenticationManager = authenticationManager(http.getSharedObject(AuthenticationConfiguration.class));
+
         http.csrf().disable();
 
         http.formLogin().disable();
 
         http.addFilterBefore(
-                new JsonUsernamePasswordAuthenticationFilter(customAuthenticationSuccessHandler,
-                        customAuthenticationFailureHandler, customAuthenticationManager),
+                authenticationFilter(authenticationManager),
                 UsernamePasswordAuthenticationFilter.class);
 
         http.authorizeHttpRequests((authorize) -> authorize
@@ -58,5 +63,23 @@ public class SecurityConfig {
                 .maxSessionsPreventsLogin(true);
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    //Filter 등록을 위한 Bean
+    @Bean
+    public JsonUsernamePasswordAuthenticationFilter authenticationFilter(AuthenticationManager authenticationManager) {
+        JsonUsernamePasswordAuthenticationFilter authenticationFilter = new JsonUsernamePasswordAuthenticationFilter(customAuthenticationSuccessHandler,
+                customAuthenticationFailureHandler, customAuthenticationManager);
+        authenticationFilter.setAuthenticationManager(authenticationManager);
+
+        SecurityContextRepository contextRepository = new HttpSessionSecurityContextRepository();
+        authenticationFilter.setSecurityContextRepository(contextRepository);
+
+        return authenticationFilter;
     }
 }
--- src/main/java/com/pythonstrup/demo/domain/auth/dto/CustomUserDetails.java
+++ src/main/java/com/pythonstrup/demo/domain/auth/dto/CustomUserDetails.java
@@ -32,7 +32,7 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public boolean isEnabled(){
-        return false;
+        return true;
     }
 
     @Override
@@ -42,16 +42,16 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 }
